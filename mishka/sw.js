046a43a8986b50cf816c6f7aed4d7edbd1a6b2a6
mar013013 <fruktodar@gmail.com>
2026-08-05 11:31:22 +0300

    Refresh Mishka v6 cache

--- mishka/sw.js
+++ mishka/sw.js
@@ -1,5 +1,5 @@
-const CACHE='mishka-pwa-v5';
-const SHELL=['./','./index.html','./app.css?v=5','./app.js?v=5','./manifest.webmanifest','./icon.svg'];
+const CACHE='mishka-pwa-v6';
+const SHELL=['./','./index.html','./app.css?v=6','./app.js?v=6','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)))});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))))});
